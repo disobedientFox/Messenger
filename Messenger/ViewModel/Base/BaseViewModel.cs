01df5e272b61a987517bdc02e4dba31cfb01cb33
Алевтина Яричевская <vkysniashka@list.ru>
2018-03-25 15:58:33 +0200

    Init commit

---
+++ Messenger/ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using PropertyChanged;
+using System.ComponentModel;
+
+namespace Messenger
+{
+    /// <summary>
+    /// A base view model that fires Property Changed events as needed
+    /// </summary>
+    //[ImplementPropertyChanged]
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The event that is fired when any child property changes its value
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ Messenger/ViewModel/WindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Messenger
+{
+    class WindowViewModel : BaseViewModel
+    {
+        #region Private member
+
+        private Window mWindow;
+
+        #endregion
+
+        #region Public Properties
+
+        public string Test { get; set; } = "My string";
+
+        #endregion
+
+        #region Constructor
+        public WindowViewModel(Window window)
+        {
+            mWindow = window;
+        }
+
+        #endregion
+    }
+}
