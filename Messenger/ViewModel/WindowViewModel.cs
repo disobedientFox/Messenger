1852f966530b405330650f5ee51997e8c433069d
Алевтина Яричевская <vkysniashka@list.ru>
2018-03-25 21:15:01 +0200

    Custom Window Chrome and Styles

--- Messenger/ViewModel/WindowViewModel.cs
+++ Messenger/ViewModel/WindowViewModel.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Messeger;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows;
+using System.Windows.Input;
 
 namespace Messenger
 {
@@ -12,11 +15,66 @@ namespace Messenger
 
         private Window mWindow;
 
+        private int mOuterMarginSize = 10;
+        private int mWindowRadius = 10;
+
         #endregion
 
         #region Public Properties
+        
+        public double WindowMinimumWidth { get; set; } = 400;
+        public double WindowMinimumHeight { get; set; } = 400;
 
-        public string Test { get; set; } = "My string";
+        public int ResizeBorder { get; set; } = 6;
+
+        public Thickness ResizeBorderThickness { get { return new Thickness(ResizeBorder + OuterMarginSize); } }
+
+        public Thickness InnerContentPadding { get { return new Thickness(ResizeBorder); } }
+
+        public int OuterMarginSize
+        {
+            get
+            {
+                return mWindow.WindowState == WindowState.Maximized ? 0 : mOuterMarginSize;
+            }
+            set
+            {
+                mOuterMarginSize = value;
+            }
+        }
+
+        public int WindowRadius
+        {
+            get
+            {
+                return mWindow.WindowState == WindowState.Maximized ? 0 : mWindowRadius;
+            }
+            set
+            {
+                mWindowRadius = value;
+            }
+        }
+
+        public Thickness OuterMarginSizeThickness { get { return new Thickness(OuterMarginSize); } }
+
+        public CornerRadius WindowCornerRadius { get { return new CornerRadius(WindowRadius); } }
+
+        public int TitleHeight { get; set; } = 42;
+
+        public GridLength TitleHeightGridLength { get { return new GridLength(TitleHeight + ResizeBorder); } }
+
+
+        #endregion
+
+        #region Commands
+
+        public ICommand MinimizeCommand { get; set; }
+
+        public ICommand MaximizeCommand { get; set; }
+
+        public ICommand CloseCommand { get; set; }
+
+        public ICommand MenuCommand { get; set; }
 
         #endregion
 
@@ -24,8 +82,43 @@ namespace Messenger
         public WindowViewModel(Window window)
         {
             mWindow = window;
+
+            mWindow.StateChanged += (sender, e) =>
+            {
+                OnPropertyChanged(nameof(ResizeBorderThickness));
+                OnPropertyChanged(nameof(OuterMarginSize));
+                OnPropertyChanged(nameof(WindowRadius));
+                OnPropertyChanged(nameof(OuterMarginSizeThickness));
+                OnPropertyChanged(nameof(WindowCornerRadius));
+            };
+            MinimizeCommand = new RelayCommand(() => mWindow.WindowState =  WindowState.Minimized);
+            MaximizeCommand = new RelayCommand(() => mWindow.WindowState ^= WindowState.Maximized);
+            CloseCommand = new RelayCommand(() => mWindow.Close());
+            MenuCommand = new RelayCommand(() => SystemCommands.ShowSystemMenu(mWindow, GetMousePosition()));
+
+            var resizer = new WindowResizer(mWindow);
         }
 
         #endregion
+
+        #region Private Helpers
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref Win32Point pt);
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+        private static Point GetMousePosition()
+        {
+            Win32Point w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+            return new Point(w32Mouse.X, w32Mouse.Y);
+        }
+
+        #endregion 
     }
 }
