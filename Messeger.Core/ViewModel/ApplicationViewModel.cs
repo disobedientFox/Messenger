44cdc04d24003138c9333520308f05e6d30b6baa
Алевтина Яричевская <vkysniashka@list.ru>
2018-04-12 15:42:07 +0200

    dependency injection (finally fixed all (one) problems)

--- Messeger.Core/ViewModel/ApplicationViewModel.cs
+++ Messeger.Core/ViewModel/ApplicationViewModel.cs
@@ -10,5 +10,7 @@ namespace Messenger.Core
     public class ApplicationViewModel : BaseViewModel
     {
         public ApplicationPage CurrentPage { get; set; } = ApplicationPage.Login;
+
+        public bool SideMenuVisible { get; set; } = false;
     }
 }
