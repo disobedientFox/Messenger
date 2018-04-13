def8e81f4798fb0b5197d1cc868d7228c582888c
Алевтина Яричевская <vkysniashka@list.ru>
2018-04-13 21:21:59 +0200

    Create chat message bubbles

---
+++ Messenger/Controls/Chat/ChatMessege/ChatMessegeListControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Messenger
+{
+    /// <summary>
+    /// Логика взаимодействия для ChatMessegeListControl.xaml
+    /// </summary>
+    public partial class ChatMessegeListControl : UserControl
+    {
+        public ChatMessegeListControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Messenger/Controls/Chat/ChatMessege/ChatMessegeListItemControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Messenger
+{
+    /// <summary>
+    /// Логика взаимодействия для ChatMessegeListItemControl.xaml
+    /// </summary>
+    public partial class ChatMessegeListItemControl : UserControl
+    {
+        public ChatMessegeListItemControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
